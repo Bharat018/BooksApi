6b359130aca03dafc09f45069bdf8feb7493e290
Bharat018 <bpedhadiya192@rku.ac.in>
2021-01-30 13:04:05 +0530

    again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mogoose = require('mongoose');
 const route = require('./route');
 
 //connect to mongodb
-mogoose.connect("mongodb+srv://Bharat:bharat02@mycluster.pz6la.mongodb.net/<Student>?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mogoose.connect("mongodb+srv://Bharat:bharat02@mycluster.pz6la.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
         app.use("/api",route)
