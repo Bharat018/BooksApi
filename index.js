2a43145df01bddfcf03ff3a73e42a52d3145cb18
Bharat018 <bpedhadiya192@rku.ac.in>
2021-01-30 12:02:56 +0530

    added port, start script ,connection string

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mogooes = require('mongoose');
 const route = require('./route');
 
 //connect to mongodb
-mogooes.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mogooes.connect("mongodb+srv://Bharat:bharat02@mycluster.pz6la.mongodb.net/<Student>?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
         app.use("/api",route)
-        app.listen(3000,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log('server started..!!')
         })
     }
