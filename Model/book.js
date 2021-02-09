2fbe0a0dbdc69c8c67b83275f8bd4a6ebfca9a77
Bharat018 <bpedhadiya192@rku.ac.in>
2021-02-09 20:10:11 +0530

    "validator, timestamp, diffdata types -> array, objects"

--- Model/book.js
+++ Model/book.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-    name:String,
-    qty:Number,
-    owner:String
-})
+    name:{
+        type:String,
+        required:true,      
+        minlength:3
+    },
+    qty:{
+        type:Number,
+        required:true,
+        min:2
+    },
+    authors:{
+        type:Array
+
+    },
+    address:{
+        add1:{
+            type:String
+        },
+        add2:{
+            type:String
+        }
+    }
+},{timestamps:true});
 
 
 module.exports = mongoose.model("Book",schema);
